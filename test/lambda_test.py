457985fce31255c02c473c845d3c4d4c1f7a3bc1
Miguel Felipe Peñuela Garzon <mpenue2@bancodebogota.com.co>
2021-06-08 11:33:01 -0500

    :+1:  add coverage tests

--- test/lambda_test.py
+++ test/lambda_test.py
@@ -1,7 +1,13 @@
 import pytest
+import json
 import sys
 sys.path.append('./utils')
 import Secrets
+import Validations
+sys.path.append('./models')
+import Row
+
+from lambda_function import *
 
 secrets_test = {
     'HOST': 'localhost', 
@@ -20,9 +26,46 @@ def test_secrets(mocker, monkeypatch):
         return_value = secrets_test
     )
 
-    secretsInstance = Secrets.SecretsUtils()
-    secrets = secretsInstance.getSecrets()
+    secrets_instance = Secrets.SecretsUtils()
+    secrets = secrets_instance.getSecrets()
+    print(secrets)
     assert secrets == secrets_test
 
+
+def test_validate_register_type_renovation():
+    row = ['C','298207','1','12','13000000','0','2','2','1','2','2','6']
+    data = Row.DataRow(row)
+    register_type = Validations.validateRegisterType(data)
+
+    assert register_type == 'RENOVACION'
+    assert data.getTipoDocumento() == 'C'
+    assert data.getDocumento() == '298207'
+    assert data.getObligacionesDelClienteCerradasU12M() == '1'
+    assert data.getMesesCuotasPagadasClientePorCredito() == '12'
+    assert data.getValorDesembolsadoPorCredito() == '13000000'
+    assert data.getMoraIntrames() == '0'
+    assert data.getClienteReestructurado() == '2'
+    assert data.getClienteCobranzasONormalizado() == '2'
+    assert data.getSolicitudesRechazasFlujoMantizU6M() == '1'
+    assert data.getSolicitudesRechazadasFlujoGPOU6M() == '2'
+    assert data.getSolicitudesPendientesEnGPO() == '2'
+    assert data.getNumeroObligacionesMayor500M() == '6'
+
+def test_validate_register_type_parallel():
+    row = ['C','298207',None,None,None,'0','2','2','1','2','2','6']
+    data = Row.DataRow(row)
+    register_type = Validations.validateRegisterType(data)
+
+    assert register_type == 'PARALELO'
+
+def test_lambda_handler():
+    print('lambda_handler test')
+    try:
+        with open('event.json') as json_file:
+            event = json.load(json_file)
+            response = lambda_handler(event)
+    except Exception as e:
+        print(e)
+        
 
 
